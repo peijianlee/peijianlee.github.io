b1f2f8421b24f475849c6522fa5e5f973cca75fe
peijianlee <lpjsteel@hotmail.com>
2022-03-18 17:22:49 +0800

    Update ResourceLoader.js

--- flappyBirdWeb/js/base/ResourceLoader.js
+++ flappyBirdWeb/js/base/ResourceLoader.js
@@ -13,13 +13,27 @@ export class ResourceLoader {
 
     onloader (callback) {
         let loadedCount = 0
+        let total = this.map.size
+        const loadingFrame = document.getElementById('loading-frame')
+        const loadingTotal = document.getElementById('loading_total')
+        loadingTotal.innerHTML = total
+        const percen = 100 / total
         for (let value of this.map.values()) {
             value.onload = () => {
-                loadedCount++
-                console.log(loadedCount)
-                if (loadedCount >= this.map.size) {
-                    callback(this.map)
-                }
+                // setTimeout(() => {
+                    loadedCount++
+                    const loadingCount = document.getElementById('loading_count')
+                    loadingCount.innerHTML = loadedCount
+                    loadingFrame.setAttribute('style', `--percen: ${percen * loadedCount}%; --translateY:0%;`)
+                    if (loadedCount >= total) {
+                        callback(this.map)
+                        loadingFrame.setAttribute('style', `--percen: ${percen * loadedCount}%; --translateY:100%;`)
+                        loadingFrame.addEventListener('transitionend', ()=> {
+                            loadingFrame.remove()
+                        })
+                    }
+                    
+                // }, loadedCount * 1000)
             }
         }
     }
